0198284d4acee673df60116bc12827becf72e8f4
邓凯元 <8181397+D-kaiyuan@user.noreply.gitee.com>
2021-01-21 09:14:03 +0800

    dky

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    
+    <router-view/>
+
+    <div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+// <<<<<<< HEAD
+// * {
+// =======
+*{
+// >>>>>>> origin/dky
+  margin: 0;
+  padding: 0;
+  font-size: 0.3rem;
+}
+// <<<<<<< HEAD
+html {
+  font-size: 100px;
+  // font-size: 0.3rem;
+}
+// =======
+ol,ul{
+  list-style: none;
+}
+i{
+  font-style: normal;
+}
+#app,html,body {
+    width: 100%;
+    height: 100%;
+}
+
+// >>>>>>> origin/dky
+</style>
